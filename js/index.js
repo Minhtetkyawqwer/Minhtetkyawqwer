398ee3dfb54810c39ada1e1bb93ff2f6d646c6ed
Minhtetkyawqwer <minhtetkyaw002@gmail.com>
2022-03-15 10:29:16 +0630

    first project

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const up=document.querySelector('.up');
+up.onclick=()=>{
+    window.scrollTo(0,0);
+}
+
+window.onscroll=()=>{
+    if(window.scrollY>=500){
+        up.style.display="block"
+    }else{
+        up.style.display="none"
+    }
+}
